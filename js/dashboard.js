b4a6ceb4c7e7fea33436d6b1fea2f618842ee87b
hortfrancis <ajhf@hotmail.co.uk>
2024-01-26 12:07:53 +0000

    Fix generic prep request to production backend

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,17 @@
-const fetchData = async () => {
-    const response = await fetch(endpointUrl);
-    return await response.json();
+/*
+    Fire a generic HTTP GET request to the root of the production backend, 
+    to 'spin it up' in advance of data requests from subsequent pages 
+*/
+const prepareBackend = async () => {
+    try {
+        const response = await fetch(endpointUrl);
+        console.log(await response.ok ? "Backend available and ready for requests." : "Backend unavailable")
+    } catch (error) {
+        console.error("Unable to connect to backend:", error);
+    }
 }
 
-// PRODUCTION ENDPOINT HERE:
+// PRODUCTION ENDPOINT
 const endpointUrl = 'https://lfa-project1-backend.onrender.com/';
+
+prepareBackend();
